f65ba4250e37e6a64943c1cb2b2f2817ccbf8043
Mel Almada <101682418+melxtear@users.noreply.github.com>
2022-11-22 23:31:13 -0300

    Cambios Funciones Crear Archivos

--- probando archivos.cpp
+++ probando archivos.cpp
@@ -227,27 +227,90 @@ Contactos* read_archivo_contactos(string a1, int* contador4) {
 	return l_cont;
 }
 
-void crear_archivo(string nombre_a1, string nombre_a2) {
-	fstream archi, archi2;
 
-	archi.open(nombre_a1, ios::out);
-	archi2.open(nombre_a2, ios::out);
-
-	if (archi.is_open() && archi2.is_open()) {
-		archi << "dni , nota1 , nota2" << endl;
-		archi << 1000 << " , " << 8 << " , " << 7 << endl;
-		archi << 10932 << " , " << 6 << " , " << 8 << endl;
-		archi2 << "dni , apellido , nombre" << endl;
-		archi2 << 10932 << " , Maradona , Diego" << endl;
-		archi2 << 1000 << " , Messi , Lionel" << endl;
+void agregar_pacientes_archivados(Pacientes*& lista_pac, Pacientes paciente, int* tamactual) {
+	*tamactual = *tamactual + 1;
+	int i = 0;
+	Pacientes* aux = new Pacientes[*tamactual];
+	while (i < *tamactual - 1 && *tamactual - 1 != 0) {
+		aux[i] = lista_pac[i];
+		i++;
 	}
+	aux[i] = paciente;
 
-	archi.close();
-	archi2.close();
+	delete[] lista_pac;
+	lista_pac = aux;
 
 	return;
 }
 
+void crear_archivo_pacientes_archivados(string nombre_a1, Pacientes*& lista_pac_archivados, int* tamactual) {
+	fstream archi, archi2;
+
+	archi.open(nombre_a1, ios::out);
+	//archi2.open(nombre_a2, ios::out);
+	int i = 0;
+
+	//&& archi2.is_open()
+	if (archi.is_open()) {
+		archi << "dni , nombre , apellido , sexo , natalicio , estado , obra_social" << endl;
+		while (i < *tamactual) {
+			//dni , nombre , apellido , sexo , natalicio , estado , obra_social
+			
+			archi << lista_pac_archivados[i].dni << " , " << lista_pac_archivados[i].nombre << " , " << lista_pac_archivados[i].apellido << " , " << lista_pac_archivados[i].sexo << " , " << lista_pac_archivados[i].natalicio << " , " << lista_pac_archivados[i].estado << " , " << lista_pac_archivados[i].id_os << "\n";
+			//archi << 10932 << " , " << 6 << " , " << 8 << endl;
+			//archi2 << "dni , apellido , nombre" << endl;
+			//archi2 << 10932 << " , Maradona , Diego" << endl;
+			//archi2 << 1000 << " , Messi , Lionel" << endl;
+			i++;
+		}
+		
+	}
+
+	archi.close();
+	//archi2.close();
+
+	return;
+}
+
+Pacientes* archivar_pacientes(Pacientes*& lista_pac_a_archivar, int* contador5) {
+	Pacientes* l_pac = new Pacientes[0];
+	int tamactual = 0;
+	for (int i = 0; i <= 100; i++) {
+		if (lista_pac_a_archivar[i].estado == "fallecido") {
+			agregar_pacientes_archivados(l_pac, lista_pac_a_archivar[i], &tamactual);
+		}
+	}
+	*contador5 = tamactual;
+	return l_pac;
+}
+
+Pacientes* read_archivo_pacientes_archivados(string a1) {
+	Pacientes* l_pac = new Pacientes[0];
+	Pacientes aux;
+	fstream fr;
+	string dummy;
+	char coma;
+	int tamact = 0;
+
+	fr.open(a1, ios::in);
+	//fr.open(a1, ios::in | ios::app);
+	if (!fr.is_open())
+		cout << "Error al leer archivo";
+	else {
+		fr >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy;
+		while (fr) {
+			fr >> aux.dni >> coma >> aux.apellido >> coma >> aux.nombre >> coma >> aux.sexo >> coma >> aux.natalicio >> coma >> aux.estado >> coma >> aux.id_os;
+			//cout << aux.nombre << '\n';
+			agregar_pacientes(l_pac, aux, &tamact);
+		}
+
+
+	}
+
+
+	return l_pac;
+}
 
 int main()
 {
@@ -262,6 +325,7 @@ int main()
 	int contador2 = 0;
 	int contador3 = 0;
 	int contador4 = 0;
+	int contador5 = 0;
 
 	Pacientes* lista;
 	lista=read_archivo_pacientes("Pacientes.csv", &contador);//con & transformo la variable en un puntero
@@ -274,11 +338,20 @@ int main()
 
 	Contactos* lista3;
 	lista3 = read_archivo_contactos("Contactos.csv", &contador4);
+	
+	Pacientes* lista4;
+	lista4 = archivar_pacientes(lista, &contador5);
+		
+	crear_archivo_pacientes_archivados("Pacientes_Archivados.csv", lista4, &contador5);
 
+	Pacientes* lista5;
+	lista5 = read_archivo_pacientes_archivados("Pacientes_Archivados.csv");
+	
 	for (int i = 0; i < contador; i++) {
 		cout << lista[i].dni << "," << lista[i].nombre << "," << lista[i].apellido << "," << lista[i].sexo << "," << lista[i].natalicio << "," << lista[i].estado << "," << lista[i].id_os << endl;
 	}
 
+	
 	for (int i = 0; i < contador2; i++) {
 		cout << lista1[i].dni_pac << "," << lista1[i].fecha_solicitado << "," << lista1[i].fecha_turno << "," << lista1[i].presento << "," << lista1[i].matricula_med << endl;
 	}
@@ -290,6 +363,13 @@ int main()
 	for (int i = 0; i < contador4; i++) {
 		cout << lista3[i].dni_pac << "," << lista3[i].telefono << "," << lista3[i].celular << "," << lista3[i].direccion << "," << lista3[i].mail << endl;
 	}
+	
+	cout << "Leyendo pacientes archivados:" << endl;
+	for (int i = 0; i < contador5; i++) {
+	  cout << lista5[i].dni << "," << lista5[i].nombre << "," << lista5[i].apellido << "," << lista5[i].sexo << "," << lista5[i].natalicio << "," << lista5[i].estado << "," << lista5[i].id_os << endl;
+	}
+
+
     //If file is not created, return error
     if (!file) {
         cout << "Error in file creation!";
